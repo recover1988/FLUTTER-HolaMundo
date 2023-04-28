3d2d43fab904c2856fa2c0ca795f25e7fdaa3328
Eric Denis Laura <ericdenislaura@gmail.com>
2023-04-28 01:59:38 -0300

    directorios

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:hello_world_app/screens/counter/counter_screen.dart';
 
 void main(List<String> args) {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Center(child: Text('Hola Mundo')),
-      )
-    );
+        debugShowCheckedModeBanner: false,
+        home: CounterScreen() // CounterScreem
+        );
   }
 }
